65da4eb8ee0ec0d5bf34011ae32274442f059f07
StanislasE <stanislas.etienne@gmail.com>
2020-11-19 10:26:04 +0100

    fait

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercice 4</title>
+</head>
+<body>
+    
+<?php
+    $age = 17 ;
+
+    if (($age >= 18) && ($age <= 120)){
+        echo 'Tu es majeur' ;
+    }
+    else {
+        echo 'Tu n\'est pas majeur' ;
+    }
+?>
+
+</body>
+</html>
